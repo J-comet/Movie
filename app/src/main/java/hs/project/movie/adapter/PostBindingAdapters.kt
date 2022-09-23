f965320ebfc079c0c9a860b808210a064f7c29ca
hyeseong <hyeseong7848@gmail.com>
2022-09-23 17:05:40 +0900

    commit

--- app/src/main/java/hs/project/movie/adapter/PostBindingAdapters.kt
+++ app/src/main/java/hs/project/movie/adapter/PostBindingAdapters.kt
@@ -3,7 +3,7 @@ package hs.project.movie.adapter
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.load
-import coil.transform.CircleCropTransformation
+import coil.transform.RoundedCornersTransformation
 import hs.project.movie.R
 
 @BindingAdapter("imageFromUrl")
@@ -11,9 +11,8 @@ fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
     if (!imageUrl.isNullOrEmpty()) {
         view.load(imageUrl) {
             crossfade(true)
-            error(R.drawable.ic_baseline_error_24)
             placeholder(R.drawable.ic_baseline_arrow_drop_down_circle_24)
-            transformations(CircleCropTransformation())
+            transformations(RoundedCornersTransformation(10f))
         }
     }
 }
